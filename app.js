545b3dc778eaa6f1899aeb716f09443c7d39fafe
Uttam28s <umoradiya31@gmail.com>
2022-03-21 16:16:30 +0530

    Add factory module in app.js

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const env = require("dotenv").config().parsed;
-const port = process.env.PORT || env.APP_PORT || 3002;
+const port = process.env.PORT || env.APP_PORT || 3003;
+const cors = require("cors");
 const passport = require("passport");
-const { roughRoutes, officeRoutes, commonRoutes } = require("./Routes/index");
+const { roughRoutes, officeRoutes, commonRoutes, factoryRoutes } = require("./Routes");
 
-const dConnection = `mongodb+srv://Uttam28s:76986Utt%40m@diamond.sswlz.mongodb.net/Diamond?retryWrites=true&w=majority`
-  // env.DB_CONNECTION +
-  // "://" +
-  // env.DB_HOST +
-  // ":" +
-  // env.DB_PORT +
-  // "/" +
-  // env.DB_DATABASE;
+const dConnection = 'mongodb+srv://Uttam28s:76986Utt%40m@diamond.sswlz.mongodb.net/Diamond?retryWrites=true&w=majority'
+
+// 'mongodb+srv://Uttam28s:76986%40Uttam@diamond.sswlz.mongodb.net/Diamond?retryWrites=true&w=majority'
+// env.DB_CONNECTION +
+// "://" +
+// env.DB_HOST +
+// ":" +
+// env.DB_PORT +
+// "/" +
+// env.DB_DATABASE;
 
 const options = {
   useCreateIndex: true,
@@ -31,21 +34,17 @@ mongoose
     throw new Error("Database credentials are invalid.");
   });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
+app.use(cors());
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use("/",)
 // app.use("/api/auth", authRoute);
 app.use("/api/rough", roughRoutes);
 app.use("/api/office", officeRoutes);
 app.use("/api/common", commonRoutes);
+app.use("/api/factory", factoryRoutes)
 
 app.use(passport.initialize());
 app.use(passport.session());
